f8e0d0dc464f2dc2495b6eda88d898cb6c9c78b9
espotorno <espotorno@ddda90ae-c870-475e-abdb-4af5ce0eebbb>
2012-09-10 13:49:13 +0000

    vuelta atras con la funcion loadRecords: necesaria para cuando se agregan registros en blanco

--- ext/ux/xpotronix/xpStore.js
+++ ext/ux/xpotronix/xpStore.js
@@ -168,6 +168,59 @@ Ext.extend( Ext.ux.xpotronix.xpStore, Ext.data.Store, {
 	acl:		{},
 	pruneModifiedRecords: true,
 
+    loadRecords : function(o, options, success){
+        var i, len;
+        
+        if (this.isDestroyed === true) {
+            return;
+        }
+        if(!o || success === false){
+            if(success !== false){
+                this.fireEvent('load', this, [], options);
+            }
+            if(options.callback){
+                options.callback.call(options.scope || this, [], options, false, o);
+            }
+            return;
+        }
+        var r = o.records, t = o.totalRecords || r.length;
+        if(!options || options.add !== true){
+            if(this.pruneModifiedRecords){
+                this.modified = [];
+            }
+            for(i = 0, len = r.length; i < len; i++){
+                r[i].join(this);
+            }
+            if(this.snapshot){
+                this.data = this.snapshot;
+                delete this.snapshot;
+            }
+            this.clearData();
+            this.data.addAll(r);
+            this.totalLength = t;
+            this.applySort();
+            this.fireEvent('datachanged', this);
+        }else{
+            var toAdd = [],
+                rec,
+                cnt = 0;
+            for(i = 0, len = r.length; i < len; ++i){
+                rec = r[i];
+                if(this.indexOfId(rec.id) > -1){
+                    this.doUpdate(rec);
+                }else{
+                    toAdd.push(rec);
+                    ++cnt;
+                }
+            }
+            this.totalLength = Math.max(t, this.data.length + cnt);
+            this.insert(0,toAdd);
+        }
+        this.fireEvent('load', this, r, options);
+        if(options.callback){
+            options.callback.call(options.scope || this, r, options, true);
+        }
+    },
 
 	update_model: function( e ) {
 
